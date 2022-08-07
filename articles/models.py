1bc57666535bcb69b9a8e46ba8b42913f68e94d3
Hamisi-Esq <hamisibriva540@gmail.com>
2022-08-07 18:24:03 +0300

    Django Admin interaction

--- articles/models.py
+++ articles/models.py
@@ -8,5 +8,9 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add= True)
 
+
+    def __str__(self):
+        return self.title
+
     #add in thumbnail later
     #add in author later
