3bc4094c843fc884f476b066048fd05c577e3119
Hamisi-Esq <hamisibriva540@gmail.com>
2022-08-12 16:34:17 +0300

    URL Tags

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 import re
 from django.shortcuts import render
 from .models import Article
@@ -6,3 +7,6 @@ from .models import Article
 def article_list (request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html',{'articles':articles})
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
