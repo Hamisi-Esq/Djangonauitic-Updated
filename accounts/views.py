bbc3f65a0a1e82592bb51e2cee6062bc9c153349
Hamisi-Esq <hamisibriva540@gmail.com>
2022-08-18 22:19:35 +0300

    Error error

--- accounts/views.py
+++ accounts/views.py
@@ -29,5 +29,5 @@ def login_view(request):
 
 def logout_view(request):
     if request.method == 'POST':
-        logout(request)
+        logout(request.POST)
         return redirect('articles:list')
