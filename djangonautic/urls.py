e132be846bbc33886df1ea96f0ab78d25d1aa24c
Hamisi-Esq <brivahamisi@gmail.com>
2022-08-22 10:41:48 +0300

    Redirecting the Homepage

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -4,12 +4,13 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.homepage),
+    path('', article_views.article_list, name="home"),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
 ]
